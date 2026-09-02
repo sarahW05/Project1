84a1e6cc36bf523189e3c8914f30284e7d14ffbb
Sarah Wafa <sarahwafa6@gmail.com>
2026-09-02 15:05:27 -0700

    created the loginscreen kt and made mainactivity first open login

--- app/src/main/java/com/example/project1_438/MainActivity.kt
+++ app/src/main/java/com/example/project1_438/MainActivity.kt
@@ -4,28 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+
 import com.example.project1_438.ui.theme.Project1438Theme
 
 class MainActivity : ComponentActivity() {
@@ -34,74 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Project1438Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LoginScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LoginScreen()
             }
-        }
-    }
-}
-
-@Composable
-fun LoginScreen(modifier: Modifier = Modifier) {
-    //variables created to store user information. They start as empty but get updated as user types
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-
-    Column(
-        modifier = modifier.fillMaxSize().padding(25.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "YourDictionary",
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold
-        )
-
-        Spacer(
-            modifier = Modifier.height(10.dp)
-        )
-
-        Text(
-            text = "Log In",
-            fontSize = 20.sp
-        )
-
-        Spacer(
-            modifier = Modifier.height(30.dp)
-        )
-
-        //this adds textbox
-
-        OutlinedTextField(
-            value = username,
-            onValueChange = {username = it},
-            label = {Text("Username")},
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(
-            modifier = Modifier.height(15.dp)
-        )
-
-        OutlinedTextField(
-            value = password,
-            onValueChange = {password = it},
-            label = {Text("Password")},
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(
-            modifier = Modifier.height(25.dp)
-        )
-
-        Button(onClick = {//login functionality
-         },
-            modifier = Modifier.fillMaxWidth()) {
-            Text("Log In")
         }
     }
 }
