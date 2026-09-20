97040eb4418aedca9047e2c23bb29e846e97e6bc
Niko-Proud <nproud98@gmail.com>
2026-09-19 18:03:21 -0700

    Made Detekt actually runnable, got jumpscared by 53 violations. Added database version migration, but can't test because I deleted the old version. added some comments.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@
 import net.ltgt.gradle.errorprone.errorprone
 //Apparently this will allow us to force pmd to run, in a custom gradle task anyway
 import org.gradle.api.plugins.quality.Pmd
+//Detekt requires this import for the custom task
+import dev.detekt.gradle.Detekt
 
 plugins {
     alias(libs.plugins.android.application)
@@ -14,6 +16,12 @@ plugins {
 
     id("net.ltgt.errorprone")
     id("com.github.spotbugs")
+
+//    Detekt
+    id("dev.detekt")
+
+//    Enable schema exporting so the database can migrate versions
+    id("androidx.room3")
 }
 
 //The necessary enabler for Errorprone to run on gradle
@@ -22,6 +30,18 @@ tasks.withType<JavaCompile>().configureEach {
 }
 
 
+room3 {
+    schemaDirectory("$projectDir/schemas")
+}
+
+
+detekt{
+    toolVersion = "2.0.0-alpha.6"
+    config.setFrom(rootProject.file("config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+}
+
+//This just doesn't matter at this point,but I'm leaving it here since it took forever to get working
 pmd {
     isConsoleOutput = true
     toolVersion = "7.26.0"
@@ -106,6 +126,15 @@ dependencies {
     pmd("net.sourceforge.pmd:pmd-kotlin:7.26.0")
 
 }
+
+//Detekt also needs a task definition in order to run directly
+// Kotlin DSL
+tasks.withType<Detekt>().configureEach {
+    reports {
+        checkstyle.required.set(true)
+    }
+}
+
 
 //Supplied by Mr. Gippity, because my bran is tired from trying to get this to work for 6 hours while the robot kept running me in circles
 //I gotta just start reading documentation myself instead of asking the hallucination machines for help explaining things
